5b04365710e8681355a814d63225210d3e5e5d34
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-02 13:48:09 -0500

    commit tree ea3d2874fc7fc3ab36f3a6cd770e0554104d59fc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :"super_hero"
+    end
+
+    post '/team' do
+        @team = params[:team]
+        erb :"team"
+    end
+
+    get '/team' do
+        erb :"team"
+    end
 
 end
